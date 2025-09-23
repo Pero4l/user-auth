e1e68c19f356faba04836af5b86d13a74b296e8b
pero4l <lilpero03@gmail.com>
2025-09-23 10:46:05 +0100

    test my user auth with password hash and it is woeking well

---
+++ controllers/file-check.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const filePath = './user.json';
+
+function checkFile(){
+    try{
+        if(!fs.existsSync(filePath)) return [];
+        const data = fs.readFileSync(filePath, 'utf-8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.log("Error reading file:", err);
+        return [];
+    }
+}
+
+
+function writeFile(users){
+    try{
+        fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+    } catch (err) {
+        console.log("Error writing file:", err);
+    }
+}
+
+
+module.exports = { checkFile, writeFile };
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const authRouter = require("./router/auth.route")
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to My User Auth API");
+})
+
+app.use("/auth", authRouter)
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`);
+});
